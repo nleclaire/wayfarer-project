56c551525c58a438dbd72a7717f6f89ca25f52aa
nleclaire <noah.leclaire@gmail.com>
2021-05-05 10:45:35 -0400

    Rendering cities for each city

--- src/app/cities/cities.component.ts
+++ src/app/cities/cities.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CitiesComponent implements OnInit {
 
+  cities = [
+    {
+      name : 'San Francisco',
+      image_url : ''
+    },
+    {
+      name : 'London',
+      image_url : ''
+    },
+    {
+      name : 'Gibraltar',
+      image_url : ''
+    },
+    {
+      name : 'Sydney',
+      image_url : ''
+    },
+    {
+      name : 'New York',
+      image_url : ''
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
